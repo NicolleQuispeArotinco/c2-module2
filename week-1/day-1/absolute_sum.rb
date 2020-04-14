11655f794ddfdf9209538012c1ea4567bfbb8ade
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-04-13 20:55:44 -0500

    Solution for Absolute sum assignment

--- week-1/day-1/absolute_sum.rb
+++ week-1/day-1/absolute_sum.rb
@@ -2,9 +2,6 @@ print("Number 1:")
 number1=Integer(gets.chomp)
 print("Number 2:")
 number2=Integer(gets.chomp)
-def add(number1, number2)
-    total=number1+number2
-end
 def absolute(number1, number2)
     if(number1+number2<0)
         return (number1+number2)*(-1)
