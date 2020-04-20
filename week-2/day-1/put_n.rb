ff7ccc228dd8f2f274a4b6235e23f95e95a73e04
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-04-20 17:34:49 -0500

    put n

---
+++ week-2/day-1/put_n.rb
@@ -0,0 +1,13 @@
+def put_n(text, times_to_print)
+    if (times_to_print.is_a? Integer )&& times_to_print>-1
+        times_to_print.times { puts text}
+    elsif !times_to_print.is_a?(Integer)
+        raise ArgumentError.new("The value is not an integer")
+    elsif times_to_print<0
+        raise ArgumentError.new("The value is negative")
+    end
+end
+
+put_n("This will be printed 5 times!", 5)
+#put_n("This will be printed 5 times!", 0.2)
+put_n("This will be printed 5 times!", -2)
