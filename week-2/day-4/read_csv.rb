5627c50f777826ef7b081ceb61f661c4d29ff6b1
NicolleQuispeArotinco <58643759+NicolleQuispeArotinco@users.noreply.github.com>
2020-04-24 09:30:13 -0500

    Update read_csv.rb

--- week-2/day-4/read_csv.rb
+++ week-2/day-4/read_csv.rb
@@ -3,7 +3,6 @@ def read_csv
   grades = File.read("grades.csv").gsub(' ', '').gsub('"', ' ')
   csv = CSV.parse(grades)
   last=csv.length
-  position = 0
   student=csv[1..last].sort{|x, y| y[7] <=> x[7]}
   for i in 0..2
     puts "#{i+1} - #{student[i][1]} with a final grade of #{student[i][7]}"
