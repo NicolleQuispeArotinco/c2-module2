e954a718747b8fdaf8191427ffa3ab7c74d498a6
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-04-27 15:52:47 -0500

    Reading grade

---
+++ week-3/day-1/reading-grade.rb
@@ -0,0 +1,13 @@
+require 'textstat'
+
+def reading_grade(text)
+    TextStat.coleman_liau_index(text).round
+end
+
+text1 = "Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard."
+text2 = "When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow. When it healed, and Jem's fears of never being able to play football were assuaged, he was seldom self-conscious about his injury. His left arm was somewhat shorter than his right; when he stood or walked, the back of his hand was at right angles to his body, his thumb parallel to his thigh."
+text3 = "It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him."
+
+puts reading_grade(text1)
+puts reading_grade(text2)
+puts reading_grade(text3)
